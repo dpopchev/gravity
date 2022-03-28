93bc2e1e553aa3c5551262af28947fed94dde26c
Dimitar Popchev <dpopchev@gmail.com>
2022-03-28 23:56:00 +0300

    rk as enums

--- lib/nrpy.py
+++ lib/nrpy.py
@@ -33,6 +33,47 @@ class CoordSystem(Enum):
     def __str__(self):
         return f'{self.value}'
 
+    def as_string(self):
+        return self.__str__()
+
+class RKmethod(Enum):
+    EULER = 'Euler'
+    RK2HEUN = 'RK2 Heun'
+    RK2MP = 'RK2 MP'
+    RK2RALSTON = 'RK2 Ralston'
+    RK3 = 'RK3'
+    RK3HEUN = 'RK3 Heun'
+    RK3RALSTON = 'RK3 Ralston'
+    SSPRK3 = 'SSPRK3'
+    RK4 = 'RK4'
+    DP5 = 'DP5'
+    DP5ALT = 'DP5alt'
+    CK5= 'CK5'
+    DP6 = 'DP6'
+    L6 = 'L6'
+    DP8 = 'DP8'
+
+    @classmethod
+    def pick(cls, system):
+        normalized = system.upper()
+        matches = (m for m in cls.__members__.items() if m[0] == normalized)
+
+        match = next(matches, None)
+        if match is None:
+            raise AttributeError('No matches')
+
+        match0 = next(matches, None)
+        if match0 is not None:
+            raise AttributeError('Too many matches')
+
+        return match[1]
+
+    def __str__(self):
+        return f'{self.value}'
+
+    def as_string(self):
+        return self.__str__()
+
 # This should be done by Makefile
 class Ccode:
     """C code output directory builder; NOTE this should be handled by Makefile
