480fdd06f0c6584c7cc3230d687c4523a1395e85
Dimitar Popchev <dpopchev@gmail.com>
2022-04-17 11:05:22 +0300

    granuall refactoring

--- lib/nrpy.py
+++ lib/nrpy.py
@@ -15,17 +15,15 @@ from dataclasses import dataclass, InitVar
 
 import pdb
 
-CCODESDIR = os.path.join("ccodesdir_default")
-
 @dataclass
-class CodesDir:
-    root: os.PathLike = CCODESDIR
-    output: os.PathLike = None
-    outputdir: InitVar[str] = 'output'
+class CcodesDir:
+    root: str = "ccodesdir_default"
+    output: str = None
+    output_name: InitVar[str] = 'output'
 
-    def __post_init__(self, outputdir):
+    def __post_init__(self, output_name):
         if self.output is None:
-            self.output = os.path.join(self.root, outputdir)
+            self.output = os.path.join(self.root, output_name)
 
     def clean(self):
         shutil.rmtree(self.root, ignore_errors=True)
@@ -39,31 +37,16 @@ class CodesDir:
 class SpatialDimension:
     value: int = 3
     dim: str = None
-    ccodesdir: CodesDir = None
 
-    def build_grid(self):
+    def build_paramter(self):
         par.set_parval_from_str('grid::DIM', self.value)
+
+    def build_dim(self):
         self.dim = par.parval_from_str('grid::DIM')
 
-    def build_find_timestep(self):
-        target = os.path.join(self.ccodesdir.root, 'find_timestep.h')
-        rfm.out_timestep_func_to_file(target)
-
-@dataclass
-class CoordSystem:
-    coord_system: str = 'Spherical'
-    domain_size: float = 32
-    sinh_width: float = 0.2
-    sinhv2_const_dr: float =0.05
-    symtp_bscale: float = 0.5
-    symmetry_axes: str = '12'
-
-    def build_numerical_grid(self):
-        par.set_parval_from_str('reference_metric::CoordSystem', self.coord_system)
-        rfm.reference_metric()
-
-    def build_symmetry(self):
-        par.set_parval_from_str('indexedexp::symmetry_axes', self.symmetry_axes)
+    def build(self):
+        self.build_paramter()
+        self.build_dim()
 
 @dataclass
 class Derivatives:
@@ -73,12 +56,44 @@ class Derivatives:
     cfl_factor: float = 0.5
     lapse_condition: str = 'OnePlusLog'
     shift_condition: str = 'GammaDriving2ndOrder_Covariant'
-    dirname: str = 'MoLtimestepping'
-    ccodesdir: CodesDir = None
 
-    def build_dir(self):
-        self.root = os.path.join(self.ccodesdir.root,self.dirname)
-        cmd.mkdir(self.root)
+@dataclass
+class CoordSystem:
+    name: str = 'Spherical'
+    domain_size: int = 32
+    sinh_width: float = 0.2
+    sinhv2_const_dr: float = 0.05
+    symtp_bscale: float = 0.5
+    derivatives: Derivatives = None
+
+    def build_reference_metric(self):
+        par.set_parval_from_str('reference_metric::CoordSystem', self.name)
+        rfm.reference_metric()
+
+    def build_fd_order(self):
+        par.set_parval_from_str('finite_difference::FD_CENTDERIVS_ORDER',
+                                self.derivatives.fd_order)
+
+    def build(self):
+        self.build_reference_metric()
+        self.build_fd_order()
+
+@dataclass
+class RungeKuttaTimesteppingCode:
+    derivatives: Derivatives = None
+    ccodesdir: CcodesDir = None
+    rk_order: int = None
+    dirname: str = 'MoLtimestepping'
+    dirpath: str = None
+    rhs_string: str = None
+    post_rhs_string: str = None
+
+    def build_rk_order(self):
+        self.rk_order = Butcher_dict[self.derivatives.rk_method]
+
+    def build_dirpath(self):
+        self.dirpath = os.path.join(self.ccodesdir.root, self.dirname)
+        cmd.mkdir(self.dirpath)
 
     def build_rhs_string(self):
         ricci_eval = 'Ricci_eval(&rfmstruct, &params, RK_INPUT_GFS, auxevol_gfs);'
@@ -90,95 +105,43 @@ class Derivatives:
         enforce_detgammahat_constraint = 'enforce_detgammahat_constraint(&rfmstruct, &params,                     RK_OUTPUT_GFS);'
         self.post_rhs_string = '\n'.join(['\n', apply_bcs_curvilinear, enforce_detgammahat_constraint, '\n'])
 
-    def build_c_code_generation(self):
-        rk_order  = Butcher_dict[self.rk_method][1]
-        self.build_dir()
+    def build_mol_c_code(self):
         self.build_rhs_string()
         self.build_post_rhs_string()
-        MoL.MoL_C_Code_Generation(self.rk_method,
+        MoL.MoL_C_Code_Generation(self.derivatives.rk_method,
                                   RHS_string = self.rhs_string,
                                   post_RHS_string = self.post_rhs_string,
-                                  outdir = self.root
+                                  outdir = self.dirpath
                                   )
-    def build_finite_difference_order(self):
-        par.set_parval_from_str('finite_difference::FD_CENTDERIVS_ORDER', self.fd_order)
-
-@dataclass
-class SimdIntrinsics:
-    ccodesdir: CodesDir = None
-    simd_source: InitVar[str] = '../nrpytutorial'
-    simd_path: InitVar[str] = os.path.join('SIMD')
-    simd_header: InitVar[str] = 'SIMD_intrinsics.h'
-    source: str = None
-    destination: str = None
-
-    def __post_init__(self, simd_source, simd_path, simd_header):
-        if self.source is None:
-            self.source = os.path.join(simd_source, simd_path, simd_header)
-
-        if self.destination is None:
-            self.destination = os.path.join(self.ccodesdir.root, simd_path)
-
-    def build_destination(self):
-        cmd.mkdir(self.destination)
 
     def build(self):
-        self.build_destination()
-        shutil.copy(self.source, self.destination)
+        self.build_rk_order()
+        self.build_dirpath()
+        self.build_mol_c_code()
 
 @dataclass
-class ScalarFieldInitialData:
-    ccodesdir: CodesDir = None
-    coord_system: InitVar[CoordSystem] = None
-    outfilename: InitVar[str] = 'SFID.txt'
-    output: str = None
-    id_family: str = "Gaussian_pulse"
-    pulse_amplitude: float = 0.4
-    pulse_center: float = 0
-    pulse_width: float = 1
-    nr: int = 30000
-    rmax_weight: InitVar[float] = 1.1
-    rmax: float = None
-
-    def __post_init__(self, coord_system, outfilename, rmax_weight ):
-        if self.output is None:
-            self.output = os.path.join( self.ccodesdir.output, outfilename )
-
-        if self.rmax is None:
-            self.rmax = coord_system.domain_size*rmax_weight
-
-    def build_initial_data(self):
-        sfid.ScalarField_InitialData(self.output,
-                                     self.id_family,
-                                     self.pulse_amplitude,
-                                     self.pulse_center,
-                                     self.pulse_width,
-                                     self.nr,
-                                     self.rmax)
-
-    def build_c_code(self):
-        sfid.NRPy_param_funcs_register_C_functions_and_NRPy_basic_defines(Ccodesdir=self.ccodesdir.root)
+class Simd:
+    ccodesdir: CcodesDir = None
+    filename: str = 'SIMD_intrinsics.h'
+    filepath: str = os.path.join('../nrpytutorial', 'SIMD')
+    filetarget: str = None
 
     def build(self):
-        self.build_initial_data()
-        self.build_c_code()
-
+        self.target = os.path.join(self.ccodesdir.root, 'SIMD')
+        cmd.mkdir(self.target)
+        shutil.copy(os.path.join(self.filepath, self.filename), self.target)
 
 def build_scalar_field_collapse():
-    codesdir = CodesDir()
-    spatial = SpatialDimension(ccodesdir=codesdir)
-    coord_system = CoordSystem()
-    derivatives = Derivatives(ccodesdir=codesdir)
-    simd = SimdIntrinsics(ccodesdir=codesdir)
-    sf_init_data = ScalarFieldInitialData(ccodesdir=codesdir,
-                                          coord_system=coord_system)
+    ccodesdir = CcodesDir()
+    spatial_dimension = SpatialDimension()
+    derivatives = Derivatives()
+    coord_system = CoordSystem(derivatives=derivatives)
+    moltimestepping = RungeKuttaTimesteppingCode(derivatives=derivatives,
+                                                 ccodesdir=ccodesdir)
+    simd = Simd(ccodesdir=ccodesdir)
 
-    codesdir.build()
-    spatial.build_grid()
-    derivatives.build_c_code_generation()
-    coord_system.build_numerical_grid()
-    derivatives.build_finite_difference_order()
+    ccodesdir.build()
+    spatial_dimension.build()
+    moltimestepping.build()
+    coord_system.build()
     simd.build()
-    coord_system.build_symmetry()
-    spatial.build_find_timestep()
-    sf_init_data.build()
