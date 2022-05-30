24cbb0be1741b5afc60a3fc54967ba9114dcfcb5
Dimitar Popchev <dimitar.popchev1@globalfoundries.com>
2022-05-30 14:57:44 -0400

    smoke test default constructor

---
+++ lib/adapters.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass
+
+@dataclass
+class CcodesDir:
+    root: str = None
+    outdir: str = None
+
+    @classmethod
+    def build(cls, root = "ccodesdir_default", outdir = 'output'):
+        _root, _outdir = map(os.path.join, (root, outdir))
+        self = cls(_root, _outdir)
+        self.make_root()
+        self.make_outdir()
+        return self
+
+    def clean(self):
+        shutil.rmtree(self.root, ignore_errors=True)
+
+    def make_root(self):
+        nrpy.cmd.mkdir(self.root)
+
+    def make_outdir(self):
+        nrpy.cmd.mkdir(self.outdir)
+
+    def make_under_root(self, destination):
+        _destination = os.path.join(self.root, destination)
+        nrpy.cmd.mkdir(_destination)
+        return _destination
--- tests/adapters/test_CcodesDir.py
+++ tests/adapters/test_CcodesDir.py
@@ -1,7 +1,21 @@
 import pytest
+from adapters import CcodesDir
 
-def test_sample():
-    assert True
+class TestEmptyCcodesDir:
+    @pytest.fixture
+    def ccodes_dir(self):
+        _ccodes_dir = CcodesDir()
+        return _ccodes_dir
+
+    def test_root_attr(self, ccodes_dir):
+        expected = None
+        acutal = ccodes_dir.root
+        assert acutal is expected
+
+    def test_outdir_attr(self, ccodes_dir):
+        expected = None
+        acutal = ccodes_dir.outdir
+        assert acutal is expected
 
 if __name__ == '__main__':
     pytest.main([__file__, '-v'])
