0247ef2fdf9fc1f97a3c42f7a9df2bb93c8f1a3e
Dimitar Popchev <dimitar.popchev1@globalfoundries.com>
2022-05-30 15:07:04 -0400

    refactor to expected stub

--- tests/adapters/test_CcodesDir.py
+++ tests/adapters/test_CcodesDir.py
@@ -1,21 +1,30 @@
+import os
 import pytest
 from adapters import CcodesDir
+from collections import namedtuple
 
-class TestEmptyCcodesDir:
+CcodesDirStub = namedtuple('CcodesDirStub',  'root outdir')
+
+class TestEmptyConstruction:
     @pytest.fixture
     def ccodes_dir(self):
         _ccodes_dir = CcodesDir()
         return _ccodes_dir
 
-    def test_root_attr(self, ccodes_dir):
-        expected = None
-        acutal = ccodes_dir.root
-        assert acutal is expected
+    @pytest.fixture
+    def expected(self):
+        _expected = CcodesDirStub(None, None)
+        return _expected
 
-    def test_outdir_attr(self, ccodes_dir):
-        expected = None
-        acutal = ccodes_dir.outdir
-        assert acutal is expected
+    def test_root_attr(self, ccodes_dir, expected):
+        attr = 'root'
+        acutal = getattr(ccodes_dir, attr)
+        assert acutal is getattr(expected, attr)
+
+    def test_outdir_attr(self, ccodes_dir, expected):
+        attr = 'outdir'
+        acutal = getattr(ccodes_dir, attr)
+        assert acutal is getattr(expected, attr)
 
 if __name__ == '__main__':
     pytest.main([__file__, '-v'])
