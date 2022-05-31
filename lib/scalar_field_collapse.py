afb903a0f61822d336c1988c7f1c05f4d50b2d29
Dimitar Popchev <dimitar.popchev1@globalfoundries.com>
2022-05-31 17:49:46 -0400

    duty message, commit staged

---
+++ lib/scalar_field_collapse.py
@@ -0,0 +1,25 @@
+import adapters
+import local_nrpy as nrpy
+
+def build_timestepping(ccodes_dir, numerical_integration):
+    rfmstruct = adapters.CcodePrototypeArgument('rfmstruct', 1)
+    params = adapters.CcodePrototypeArgument('params', 1)
+    rk_input_gfs = adapters.CcodePrototypeArgument('RK_INPUT_GFS', 0)
+    rk_output_gfs = adapters.CcodePrototypeArgument('RK_OUTPUT_GFS', 0)
+    auxevol_gfs = adapters.CcodePrototypeArgument('auxevol_gfs', 0)
+    bcstruct = adapters.CcodePrototypeArgument('bcstruct', 1)
+
+    ricc_eval = adapters.CcodePrototype(
+        name = 'Ricci_eval',
+        arguments = (rfmstruct, params, rk_input_gfs, auxevol_gfs)
+    )
+    rhs_eval = adapters.CcodePrototype(
+        name = 'rhs_eval',
+        arguments = (rfmstruct, params, auxevol_gfs, rk_input_gfs, rk_output_gfs)
+    )
+
+def build():
+    ccodes_dir = adapters.CcodesDir.build()
+    dim = adapters.InterfaceParameter.build('grid::DIM', 3)
+    coord_system = adapters.CoordSystem.build_spherical()
+    numerical_integration = adapters.NumericalIntegration.build()
