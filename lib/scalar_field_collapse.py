053505629496214fc11f6090f66735d529f24292
Dimitar Popchev <dimitar.popchev1@globalfoundries.com>
2022-07-04 16:48:02 -0400

    duty message, stage and commit

--- lib/scalar_field_collapse.py
+++ lib/scalar_field_collapse.py
@@ -6,41 +6,7 @@ from ccode_builders import build_scalar_field_initial_data_ccode_generator
 from ccode_builders import build_param_funcs_basic_defines_scalar_field
 from ccode_builders import build_converter_adm_bssn_init_data
 
-def build():
-    ccodes_dir = adapters.CcodesDir.build()
-    dim = adapters.InterfaceParameter.build('grid::DIM', 3)
-    coord_system = adapters.CoordSystem.build_spherical()
-    numerical_integration = adapters.NumericalIntegration.build()
-
-    timestepping_ccode_generator = build_timestepping_ccode_generator(ccodes_dir, numerical_integration)
-    timestepping_ccode_generator.doit()
-
-    reference_metric = adapters.InterfaceParameter.build("reference_metric::CoordSystem", str(coord_system.name))
-    nrpy.rfm.reference_metric()
-
-    finite_difference = adapters.InterfaceParameter.build('finite_difference::FD_CENTDERIVS_ORDER', numerical_integration.fd_order)
-
-    simd_src = os.path.join('../nrpytutorial/SIMD/', 'SIMD_intrinsics.h')
-    simd_dst = ccodes_dir.make_under_root('SIMD')
-    nrpy.shutil.copy(simd_src,simd_dst)
-
-    indexedexp = adapters.InterfaceParameter.build("indexedexp::symmetry_axes", "12")
-
-    find_timestep_header = ccodes_dir.make_under_root('find_timestep.h', is_dir=False)
-    nrpy.rfm.out_timestep_func_to_file(find_timestep_header)
-
-    scalar_field_initial_data = build_scalar_field_initial_data_ccode_generator(ccodes_dir, coord_system)
-    scalar_field_initial_data.doit()
-
-    param_funcs_basic_defines_scalar_field = build_param_funcs_basic_defines_scalar_field(ccodes_dir)
-    param_funcs_basic_defines_scalar_field.doit()
-
-    converter_adm_bssn_init_data = build_converter_adm_bssn_init_data(ccodes_dir, coord_system)
-    converter_adm_bssn_init_data.doit()
-
-    lapse_evolution_option = adapters.InterfaceParameter.build('BSSN.BSSN_gauge_RHSs::LapseEvolutionOption', numerical_integration.lapse_condition)
-    shift_evolution_option = adapters.InterfaceParameter.build('BSSN.BSSN_gauge_RHSs::ShiftEvolutionOption', numerical_integration.shift_condition)
-
+def build_bssn_rhs_symbolic_expressions(ccodes_dir, numerical_integration, dim):
     print("Generating symbolic expressions for BSSN RHSs...")
 
     # Enable rfm_precompute infrastructure, which results in
@@ -130,3 +96,93 @@ def build():
     enable_rfm_precompute = adapters.InterfaceParameter.build("reference_metric::enable_rfm_precompute","False")
     nrpy.rfm.ref_metric__hatted_quantities()
     print("Finished BSSN symbolic expressions")
+
+    return betaU
+
+def build_bssn_plus_scalarfield_rhss(ccodes_dir, betaU):
+    rfm_coord_system = nrpy.par.parval_from_str("reference_metric::CoordSystem")
+    print(f"Generating C code for BSSN RHSs in {rfm_coord_system} coordinates.")
+
+    # Construct the left-hand sides and right-hand-side expressions for all BSSN RHSs
+    lhs_names = [        "alpha",       "cf",       "trK",         "sf",         "sfM"   ]
+    rhs_exprs = [nrpy.gaugerhs.alpha_rhs, nrpy.rhs.cf_rhs, nrpy.rhs.trK_rhs, nrpy.sfrhs.sf_rhs, nrpy.sfrhs.sfM_rhs]
+
+    for i in range(3):
+        lhs_names.append(        "betU"+str(i))
+        rhs_exprs.append(nrpy.gaugerhs.bet_rhsU[i])
+        lhs_names.append(   "lambdaU"+str(i))
+        rhs_exprs.append(nrpy.rhs.lambda_rhsU[i])
+        lhs_names.append(        "vetU"+str(i))
+        rhs_exprs.append(nrpy.gaugerhs.vet_rhsU[i])
+        for j in range(i,3):
+            lhs_names.append(   "aDD"+str(i)+str(j))
+            rhs_exprs.append(nrpy.rhs.a_rhsDD[i][j])
+            lhs_names.append(   "hDD"+str(i)+str(j))
+            rhs_exprs.append(nrpy.rhs.h_rhsDD[i][j])
+
+    # Sort the lhss list alphabetically, and rhss to match.
+    #   This ensures the RHSs are evaluated in the same order
+    #   they're allocated in memory:
+    lhs_names,rhs_exprs = [list(x) for x in zip(*sorted(zip(lhs_names,rhs_exprs), key=lambda pair: pair[0]))]
+
+    # Declare the list of lhrh's
+    BSSN_evol_rhss = []
+    for var in range(len(lhs_names)):
+        BSSN_evol_rhss.append(nrpy.lhrh(lhs=nrpy.gri.gfaccess("rhs_gfs",lhs_names[var]),rhs=rhs_exprs[var]))
+
+    # Set up the C function for the BSSN RHSs
+    desc="Evaluate the BSSN RHSs"
+    name="rhs_eval"
+    outfile_header = ccodes_dir.make_under_root(f'{name}.h', is_dir=False)
+    nrpy.outCfunction(
+        outfile  = outfile_header,
+        desc=desc,
+        name=name,
+        params   = """rfm_struct *restrict rfmstruct,const paramstruct *restrict params,
+                      const REAL *restrict auxevol_gfs,const REAL *restrict in_gfs,REAL *restrict rhs_gfs""",
+        body     = nrpy.fin.FD_outputC("returnstring",BSSN_evol_rhss, params="outCverbose=False,enable_SIMD=True",
+                                  upwindcontrolvec=betaU),
+        loopopts = "InteriorPoints,enable_SIMD,enable_rfm_precompute")
+
+    print("Finished BSSN_RHS C codegen")
+    return
+
+def build():
+    ccodes_dir = adapters.CcodesDir.build()
+    dim = adapters.InterfaceParameter.build('grid::DIM', 3)
+    coord_system = adapters.CoordSystem.build_spherical()
+    numerical_integration = adapters.NumericalIntegration.build()
+
+    timestepping_ccode_generator = build_timestepping_ccode_generator(ccodes_dir, numerical_integration)
+    timestepping_ccode_generator.doit()
+
+    reference_metric = adapters.InterfaceParameter.build("reference_metric::CoordSystem", str(coord_system.name))
+    nrpy.rfm.reference_metric()
+
+    finite_difference = adapters.InterfaceParameter.build('finite_difference::FD_CENTDERIVS_ORDER', numerical_integration.fd_order)
+
+    simd_src = os.path.join('../nrpytutorial/SIMD/', 'SIMD_intrinsics.h')
+    simd_dst = ccodes_dir.make_under_root('SIMD')
+    nrpy.shutil.copy(simd_src,simd_dst)
+
+    indexedexp = adapters.InterfaceParameter.build("indexedexp::symmetry_axes", "12")
+
+    find_timestep_header = ccodes_dir.make_under_root('find_timestep.h', is_dir=False)
+    nrpy.rfm.out_timestep_func_to_file(find_timestep_header)
+
+    scalar_field_initial_data = build_scalar_field_initial_data_ccode_generator(ccodes_dir, coord_system)
+    scalar_field_initial_data.doit()
+
+    param_funcs_basic_defines_scalar_field = build_param_funcs_basic_defines_scalar_field(ccodes_dir)
+    param_funcs_basic_defines_scalar_field.doit()
+
+    converter_adm_bssn_init_data = build_converter_adm_bssn_init_data(ccodes_dir, coord_system)
+    converter_adm_bssn_init_data.doit()
+
+    lapse_evolution_option = adapters.InterfaceParameter.build('BSSN.BSSN_gauge_RHSs::LapseEvolutionOption', numerical_integration.lapse_condition)
+    shift_evolution_option = adapters.InterfaceParameter.build('BSSN.BSSN_gauge_RHSs::ShiftEvolutionOption', numerical_integration.shift_condition)
+
+    betaU = build_bssn_rhs_symbolic_expressions(ccodes_dir, numerical_integration, dim)
+    build_bssn_plus_scalarfield_rhss(ccodes_dir, betaU)
+
+    return
